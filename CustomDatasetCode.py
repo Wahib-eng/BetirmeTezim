829bcd8d207e4fd676a8aa64b5cc09fb80b62617
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2024-06-03 15:15:13 +0300

    Delete CustomDatasetCode.py

--- CustomDatasetCode.py
+++
@@ -1,5 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="ImxMauUX41hTySVncMrO")
-project = rf.workspace("maximilian-sittinger").project("insect_detect_detection")
-version = project.version(7)
-dataset = version.download("yolov8")
